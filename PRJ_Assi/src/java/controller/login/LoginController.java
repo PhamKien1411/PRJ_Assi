21a62655eea2bdacd09bd34730a88bf3cca76cb9
PhamKien1411 <phamkien715@gmail.com>
2025-02-21 22:26:19 +0700

    Create basic login page
    tạo trang nhưng vẫn đang bị lỗi cần fix lại

---
+++ PRJ_Assi/src/java/controller/login/LoginController.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+package controller.login;
+
+import dal.DBContextUsers;
+import data.User;
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+/**
+ *
+ * @author ADM
+ */
+@WebServlet(name="LoginController", urlPatterns={"/login"})
+public class LoginController extends HttpServlet {
+   
+    /** 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    
+  
+   
+
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        DBContextUsers db = new DBContextUsers();
+        User user = db.get(username, password);
+        
+        
+        
+        if(user != null){
+            HttpSession session = request.getSession();
+            session.getAttribute("user",user);
+            response.sendRedirect("welcome");
+        }else{
+            response.getWriter().println("Login Failse! Please try again");
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+     request.getRequestDispatcher("view/Login.html").forward(request, response);
+    } 
+    
+
+}
